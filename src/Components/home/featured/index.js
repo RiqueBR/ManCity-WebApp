81c6bb1e8cc9b5575385bc887ad12cc1c68e3783
Henrique Pereira Batista <h.p.batista20@gmail.com>
2018-12-22 16:29:34 +0000

    first animations ready, background colour

--- src/Components/home/featured/index.js
+++ src/Components/home/featured/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Stripes from './Stripes';
+import Text from './Text'
+
 
 const Featured = () => {
-    return (
-        <div className="featured_wrapper">
-            some text
+    return ( 
+        <div className = "featured_wrapper" >
+        <Stripes />
+        <Text />
         </div>
     );
 };
